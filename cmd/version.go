b2775e978b56338f0282bc6fcc626e26849b9104
Mario Finelli <mario@finel.li>
2022-05-14 18:19:26 +0200

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+)
+
+import "github.com/spf13/cobra"
+
+var VERSION string = "1.0.0"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the program version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("rush version %s\n", VERSION)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
